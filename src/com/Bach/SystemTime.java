36f7f782392cb418ff5eefae1c3498f5f212f027
Hellu Shiro! <katobmt107@gmail.com>
2019-10-29 08:25:53 +0700

    2

---
+++ src/com/Bach/SystemTime.java
@@ -0,0 +1,11 @@
+package com.Bach;
+
+import java.util.Date;
+import java.util.zip.DataFormatException;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
